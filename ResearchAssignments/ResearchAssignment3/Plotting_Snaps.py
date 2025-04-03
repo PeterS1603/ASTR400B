533d86fb0e1c3af0f896e262e76d7d8ad4bfa8a8
Peter Shea <petershea@Peters-MacBook-Air.local>
2025-04-02 20:16:45 -0700

    Added interactive plotting to select and record particle indicies within user defined region

--- ResearchAssignments/ResearchAssignment3/Plotting_Snaps.py
+++ ResearchAssignments/ResearchAssignment3/Plotting_Snaps.py
@@ -10,6 +10,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 import matplotlib.patches as patches
+from matplotlib.widgets import RectangleSelector
 
 
 import numpy as np
@@ -80,11 +81,25 @@ def RotateFrame(posI,velI):
     return pos, vel
 
 def Greatest_Jacobi_Radius(R_12,R_13,M2,M3,M1):
+    '''Function calculates Jacobi Radius due to two objects and returns 
+    whichever is greatest
+    
+    Parameters:
+        R_12: float [kpc]; distance from object 1 to 2
+        R_13: float [kpc]; distance from object 1 to 3
+        M1: float [Msun]; mass of object 1
+        M2: float [Msun]; mass of object 2
+        M3: float [Msun]; mass of object 3
+        
+    Returns:
+        R_j1?: float [kpc]; Jacobi radius measured from the COM of Object 1
+    '''
     
     # calculates jacobi radius for both otehr objects
     R_j12 = R_12 * (M2 / (2*M1))**(1/3)
     R_j13 = R_13 * (M3 / (2*M1))**(1/3)
     
+    # returns the greatest of the two radii
     if R_j12 > R_j13:
         return R_j12
     else:
@@ -108,7 +123,18 @@ M33_orbit = readOrbit('Orbits/Orbit_M33.txt')
 MW_M31_relPos = relative(MW_orbit[1],M31_orbit[1])
 M33_M31_relPos = relative(M31_orbit[1],M33_orbit[1])
 
-def Density_plot(galaxy, snap):
+def Density_plot_Auto(galaxy, snap):
+    '''This function takes a list of galaxy names and snap numbers and creates 
+    a face on density plot of the galaxy that snap number.
+    
+    Parameters:
+        galaxy: ndarray; array of strings for which galaxies to create plots for
+            ex. "MW" or "M31"
+        snap: ndarray; array of integers for snap ids of the simulation state
+    
+    Returns: 
+        None
+    '''
     # Create a COM of object for given galaxy
     COMD = CenterOfMass(f"{galaxy}/{galaxy}_{snap}.txt",2)
     
@@ -126,16 +152,94 @@ def Density_plot(galaxy, snap):
     yD = COMD.y - COMP[1].value 
     zD = COMD.z - COMP[2].value 
     
-    # total magnitude
+    # total magnitude to be used with jacobi radius to determine if particle is beyond
     rtot = np.sqrt(xD**2 + yD**2 + zD**2)
     
     # Determine velocities of disk particles relatiev to COM motion
     vxD = COMD.vx - COMV[0].value 
     vyD = COMD.vy - COMV[1].value 
     vzD = COMD.vz - COMV[2].value 
+
     
-    # total velocity 
-    vtot = np.sqrt(vxD**2 + vyD**2 + vzD**2)
+    # Arrays for r and v 
+    r = np.array([xD,yD,zD]).T # transposed 
+    v = np.array([vxD,vyD,vzD]).T
+    
+    # compute the rotated position and velocity vectors
+    rn, vn = RotateFrame(r,v)
+    
+    # Hook for implementing Jacobi radius to the plot
+    """
+    R_MW_M31 = MW_M31_relPos[int(snap/5)]
+    R_M33_M31 = M33_M31_relPos[int(snap/5)]
+    
+    R_j = Greatest_Jacobi_Radius(R_MW_M31, R_M33_M31, M_MW, M_M33, M_M31)
+    print(f'Jacobi Radius = {R_j} [kpc]')
+    """
+    # Rotated Disk - FACE ON
+    # Disk Density 
+    fig, ax = plt.subplots(figsize=(11, 10))
+                  #x       y
+    ax.hist2d(rn[:,0],rn[:,1], bins=800, norm=LogNorm(), cmap='plasma')
+    
+    
+    #   Hook for implementing Jacobi radius
+    #circle = patches.Circle((0,0), R_j, edgecolor='k', facecolor='none')
+    # Add the circle to the axes
+    #ax.add_patch(circle)
+    
+    # Add axis labels
+    ax.set_xlabel('x [kpc]', fontsize=22)
+    ax.set_ylabel('y [kpc]', fontsize=22)
+    ax.set_title(f'{galaxy} Face On Density; Snap={snap}', fontsize=28)
+    
+    # set axis limits
+    ax.set_ylim(-150,150)
+    ax.set_xlim(-150,150)
+    
+    ax.set_aspect('equal')
+    
+    # saves plot
+    fig.savefig(f"{galaxy}_Density_Plots/{galaxy}_{snap}.png")
+
+def Density_plot_Manual(galaxy, snap):
+    '''This function takes a single galaxy and snap number to create a face on
+    density plot of the galaxy at that snap number. The plot created needs to 
+    be created in a window and is interactive. Dragging a box within the plot 
+    selects particles within it and writes their indicies to a txt file. 
+    
+    Parameters:
+        galaxy: string; a string for which galaxies to create a plot for ex. 
+            "MW" or "M31"
+        snap: int; an integer for the snap id of the simulation state
+    
+    Returns: 
+        None
+    '''
+    # Create a COM of object for given galaxy
+    COMD = CenterOfMass(f"{galaxy}/{galaxy}_{snap}.txt",2)
+    
+    # Compute COM using disk particles
+    if galaxy != 'M33':
+        COMP = COMD.COM_P(volDec,delta)
+        # Uses volDec_M33 = 4 for M33
+    else:
+        COMP = COMD.COM_P(volDec_M33,delta)
+    
+    COMV = COMD.COM_V(COMP[0],COMP[1],COMP[2])
+    
+    # Determine positions of disk particles relative to COM 
+    xD = COMD.x - COMP[0].value 
+    yD = COMD.y - COMP[1].value 
+    zD = COMD.z - COMP[2].value 
+    
+    # total magnitude to be used with jacobi radius to determine if particle is beyond
+    rtot = np.sqrt(xD**2 + yD**2 + zD**2)
+    
+    # Determine velocities of disk particles relatiev to COM motion
+    vxD = COMD.vx - COMV[0].value 
+    vyD = COMD.vy - COMV[1].value 
+    vzD = COMD.vz - COMV[2].value 
     
     # Arrays for r and v 
     r = np.array([xD,yD,zD]).T # transposed 
@@ -175,18 +279,66 @@ def Density_plot(galaxy, snap):
     
     ax.set_aspect('equal')
     
-    fig.savefig(f"{galaxy}_Density_Plots/{galaxy}_{snap}.png")
+    selected_particles = []
+
+    def on_select(eclick, erelease):
+        '''This function interfaces with the interactive matplotlib plot 
+        selecting particles within a box defined by clicking and dragging on 
+        the plot
+        
+        Parameters:
+            eclick: button input; pressing and holding the mouse button
+            erelease: button input; releasing the held mouse button
+        
+        Returns:
+            None
+        '''
+        global selected_particles  
+        x_min, x_max = sorted([eclick.xdata, erelease.xdata])
+        y_min, y_max = sorted([eclick.ydata, erelease.ydata])
+
+        # Find all particles within the selected region
+        mask = (rn[:, 0] >= x_min) & (rn[:, 0] <= x_max) & (rn[:, 1] >= y_min) & (rn[:, 1] <= y_max)
+        selected_particles = np.where(mask)[0]  # Get the indices of selected particles
+
+        # Defines filename to 
+        filename = f"{galaxy}_Density_Plots/{galaxy}_{snap}_SelectedParticles.txt"
+    
+        # Save the selected indices to a text file
+        np.savetxt(filename, selected_particles, fmt="%d")
+        
+        # Prints number of selected particles
+        print(f'Selected {len(selected_particles)} particles')
+    
+    rect_selector = RectangleSelector(ax, on_select, useblit=True, button=[1], interactive=True)
+    rect_selector.set_active(True)
+    
+    plt.show(block=True)
+
 
 galaxies = np.array(["MW","M31"])
 IDs = find_close_encounter_IDs()
 
+Density_plot_Manual("M31", 470)
+
 #for galaxy in galaxies:
 #    for snap in IDs:
-#        Density_plot(galaxy, int(snap))
+#        Density_plot_Auto(galaxy, int(snap))
+#        print(f'Plotting {galaxy} {snap}')
 
+ 
+def Phase_plot_Auto(galaxy, snap):
+    '''This function takes a list of galaxy names and snap numbers and creates 
+    a face on phase plot of the galaxy that snap number.
     
+    Parameters:
+        galaxy: ndarray; array of strings for which galaxies to create plots for
+            ex. "MW" or "M31"
+        snap: ndarray; array of integers for snap ids of the simulation state
     
-def Phase_plot(galaxy, snap):
+    Returns: 
+        None
+    '''
     # Create a COM of object for given galaxy
     COMD = CenterOfMass(f"{galaxy}/{galaxy}_{snap}.txt",2)
     
@@ -243,15 +395,129 @@ def Phase_plot(galaxy, snap):
     # Add axis labels
     ax.set_xlabel('x [kpc]', fontsize=22)
     ax.set_ylabel('yv [km/s]', fontsize=22)
-    ax.set_title(f'{galaxy} Face On Density; Snap={snap}', fontsize=28)
+    ax.set_title(f'{galaxy} Face On Phase; Snap={snap}', fontsize=28)
     
     #set axis limits
     ax.set_xlim(-150,150)
     
     fig.savefig(f"{galaxy}_Phase_Plots/{galaxy}_{snap}.png")
 
-galaxies = np.array(["M31"])
-for galaxy in galaxies:
-    for snap in IDs:
-        Phase_plot(galaxy, int(snap))
+
+def Phase_plot_Manual(galaxy, snap):
+    '''This function takes a single galaxy and snap number to create a face on
+    phase plot of the galaxy at that snap number. The plot created needs to be 
+    created in a window and is interactive. Dragging a box within the plot 
+    selects particles within it and writes their indicies to a txt file. 
+    
+    Parameters:
+        galaxy: string; a string for which galaxies to create a plot for ex. 
+            "MW" or "M31"
+        snap: int; an integer for the snap id of the simulation state
+    
+    Returns: 
+        None
+    '''
+    # Create a COM of object for given galaxy
+    COMD = CenterOfMass(f"{galaxy}/{galaxy}_{snap}.txt",2)
+    
+    # Compute COM using disk particles
+    if galaxy != 'M33':
+        COMP = COMD.COM_P(volDec,delta)
+        # Uses volDec_M33 = 4 for M33
+    else:
+        COMP = COMD.COM_P(volDec_M33,delta)
+    
+    COMV = COMD.COM_V(COMP[0],COMP[1],COMP[2])
+    
+    # Determine positions of disk particles relative to COM 
+    xD = COMD.x - COMP[0].value 
+    yD = COMD.y - COMP[1].value 
+    zD = COMD.z - COMP[2].value 
+    
+    # total magnitude
+    rtot = np.sqrt(xD**2 + yD**2 + zD**2)
+    
+    # Determine velocities of disk particles relatiev to COM motion
+    vxD = COMD.vx - COMV[0].value 
+    vyD = COMD.vy - COMV[1].value 
+    vzD = COMD.vz - COMV[2].value 
+    
+    # total velocity 
+    vtot = np.sqrt(vxD**2 + vyD**2 + vzD**2)
+    
+    # Arrays for r and v 
+    r = np.array([xD,yD,zD]).T # transposed 
+    v = np.array([vxD,vyD,vzD]).T
+    
+    # compute the rotated position and velocity vectors
+    rn, vn = RotateFrame(r,v)
+    
+    ### Plotting
+    fig = plt.figure(figsize=(11,10))
+    ax = plt.subplot(111)
+    
+    # Plot 2D Histogram for one component of  Pos vs Vel 
+    # ADD HERE
+    ax.hist2d(rn[:,0],vn[:,1], bins=800, norm=LogNorm(), cmap='plasma')
+    # Hook to implement Mass Profile circular velocity 
+    '''
+    # Mass Profile
+    MP = MassProfile(galaxy,snap)
+    rr = np.arange(0.01, 100.1, 0.1)*u.kpc
+    Vcirc = MP.circularVelocityTotal(rr)
+    
+    # Overplot Circular Velocity from the MassProfile Code
+    plt.plot(rr, Vcirc, color='k')
+    plt.plot(-rr,-Vcirc, c='k')
+    '''
+    # Add axis labels
+    ax.set_xlabel('x [kpc]', fontsize=22)
+    ax.set_ylabel('yv [km/s]', fontsize=22)
+    ax.set_title(f'{galaxy} Face On Phase; Snap={snap}', fontsize=28)
+    
+    #set axis limits
+    ax.set_xlim(-150,150)
+    
+    selected_particles = []
+
+    def on_select(eclick, erelease):
+        '''This function interfaces with the interactive matplotlib plot 
+        selecting particles within a box defined by clicking and dragging on 
+        the plot
+        
+        Parameters:
+            eclick: button input; pressing and holding the mouse button
+            erelease: button input; releasing the held mouse button
+        
+        Returns:
+            None
+        '''
+        global selected_particles  
+        x_min, x_max = sorted([eclick.xdata, erelease.xdata])
+        y_min, y_max = sorted([eclick.ydata, erelease.ydata])
+
+        # Find all particles within the selected region
+        mask = (rn[:, 0] >= x_min) & (rn[:, 0] <= x_max) & (vn[:, 1] >= y_min) & (vn[:, 1] <= y_max)
+        selected_particles = np.where(mask)[0]  # Get the indices of selected particles
+
+        # Defines filename to 
+        filename = f"{galaxy}_Phase_Plots/{galaxy}_{snap}_SelectedParticles.txt"
+    
+        # Save the selected indices to a text file
+        np.savetxt(filename, selected_particles, fmt="%d")
+        
+        # Prints number of selected particles
+        print(f'Selected {len(selected_particles)} particles')
+    
+    rect_selector = RectangleSelector(ax, on_select, useblit=True, button=[1], interactive=True)
+    rect_selector.set_active(True)
+    
+    plt.show(block=True)
+
+#Phase_plot_Manual("M31", 470)
+
+#for galaxy in galaxies:
+#    for snap in IDs:
+#        Phase_plot_Auto(galaxy, int(snap))
+#        print(f'Plotting {galaxy} {snap}')
 
